0fb9008642de5c42611528ee1bceb402da815575
Shanelka Pramuditha <99pramuditha@gmail.com>
2024-02-20 03:23:53 +0530

    Update BACKEND_URL in config file

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -2,7 +2,8 @@ let configs;
 
 // database uri
 configs = {
-   BACKEND_URL: import.meta.env.VITE_BACKEND_URL
+   // BACKEND_URL: import.meta.env.VITE_BACKEND_URL
+   BACKEND_URL: 'http://20.255.61.126:5001'
 };
 
 export default configs;
