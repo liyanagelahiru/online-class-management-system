a167241b71b74c9b65c2f2f2b5e421e993399237
Shanelka Pramuditha <99pramuditha@gmail.com>
2024-03-16 00:05:11 +0530

    Refactor Footer component and update Tailwind CSS config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,9 @@ import daisyui from 'daisyui';
 export default {
    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
    theme: {
+      colors: {
+         'silver-mist': '#F3F4F6'
+      },
       extend: {
          fontFamily: {
             OpenSans: ['Open Sans', 'sans-serif'],
