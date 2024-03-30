4659c4d41a69e6cb07cef027bfadf086e9df9ca5
GrimDeath <madushansasmitha@gmail.com>
2024-03-31 01:51:52 +0530

    Live class back end half created (thw chuttak haduw)

--- backend/src/api/controllers/LiveClass/LiveclassController.js
+++ backend/src/api/controllers/LiveClass/LiveclassController.js
@@ -1,4 +1,4 @@
-import LIVECLASS from "../../models/liveSessions";
+import LIVECLASS from "../../models/liveSessions.js";
 
 // Controller to create a new Class Session
 export async function createLive(req, res) {
@@ -14,5 +14,62 @@ export async function createLive(req, res) {
        res.status(200).json({ massage: 'Session Created Successfully', paper });
     } catch (error) {
        res.status(500).json({ error: 'Faild To Create A Session.' });
+    }
+ }
+
+ // Controller to edit a paper
+export async function editLive(req, res) {
+    const { sessionId, sessionName, sessionTime, description, link } = req.body;
+ 
+    try {
+       const updateFields = {};
+       if (sessionName !== undefined) {
+          updateFields.sessionName = sessionName;
+       }
+       if (sessionTime !== undefined) {
+          updateFields.sessionTime = sessionTime;
+       }
+       if (description !== undefined) {
+        updateFields.description = description;
+        }
+        if (link !== undefined) {
+            updateFields.link = link;
+         }
+ 
+       const liveclass = await LIVECLASS.findByIdAndUpdate(sessionId, updateFields, {
+          new: true
+       });
+ 
+       if (!liveclass) {
+          return res.status(404).json({ error: 'Session not found.' });
+       }
+       res.json({ message: 'Session updated successfully.' });
+    } catch (error) {
+       console.log(error);
+       res.status(500).json({ error: 'Failed to update Session.' });
+    }
+ }
+
+ // Controller to delete a Session
+export async function deleteLive(req, res) {
+    const id = req.body.id;
+ 
+    try {
+       // Find paper to be deleted and get its paperNumber
+       const LiveToDelete = await LIVECLASS.findById(id);
+ 
+       if (!LiveToDelete) {
+          res.status(404).json({ message: 'paper deleted successfully.' });
+       }
+ 
+       // Delete paper
+       await PAPER.findByIdAndDelete(id);
+
+       const quizes = await QUIZ.deleteMany({ paperId: id });
+ 
+       res.json({ message: 'paper deleted successfully.' });
+    } catch (error) {
+       console.log(error);
+       res.status(500).json({ error: 'Failed to delete paper.' });
     }
  }
