d65e07c6e962b39d85ef65b3ec7889ccabe59fbe
Shanelka Pramuditha <99pramuditha@gmail.com>
2024-04-24 18:00:48 +0530

    Refactor UpdateExam component import in UpdateExam.jsx

--- frontend/src/pages/UpdateExam/UpdateExam.jsx
+++ frontend/src/pages/UpdateExam/UpdateExam.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 import { FaTrashAlt, FaEdit } from 'react-icons/fa';
