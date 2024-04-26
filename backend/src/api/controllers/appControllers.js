8c13e53e144d42b958199da14c3a332e61f131c1
DimuthuAro <DimuthuAro@gmail.com>
2024-04-26 11:14:45 +0530

    Update 3 commit 2

--- backend/src/api/controllers/appControllers.js
+++ backend/src/api/controllers/appControllers.js
@@ -215,3 +215,76 @@ export async function resetPassword(req, res) {
       return res.status(401).send({ error });
    }
 }
+
+
+
+/* Funtion CreateUser
+ * Description - Create New User by Admin
+ * route `Post` - /users
+ * access public --- */
+export async function CreateUser(req, res) {
+    //Get Form Details From Request
+    const { Fname, Lname, email, role, gender, mobilenumber, password } = req.body
+
+    //Check for empty feilds
+    if (!Fname || !Lname || !email || !role || !gender || !mobilenumber || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+      }
+
+    // Check if user exists
+    const userExists = await User.findOne({ email })
+
+    //Return Err If User Exsist
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+      }
+    
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = await User.create({
+    Fname,  Lname,  email,  role,   gender, mobilenumber,
+    password: hashedPassword,
+    })
+
+    if (user) {
+        res.status(201).json({
+          _id: user.id,
+          name: user.name,
+          email: user.email,
+          token: generateToken(user._id),
+        })
+      } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+      }
+
+}
+
+/* Funtion CreateUser
+ * Description - Create New User by Admin
+ * route `Post` - /users
+ * access public --- */
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+  
+    // Check for user email
+    const user = await User.findOne({ email })
+  
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      })
+    } else {
+      res.status(400)
+      throw new Error('Invalid credentials')
+    }
+  })
+  
